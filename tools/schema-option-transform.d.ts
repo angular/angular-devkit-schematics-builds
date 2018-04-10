40eede09f2ce54566de7fde2a2085e0cdc847813
Angular Builds <circleci@angular.io>
2018-04-10 00:37:43 +0000

    1f6a845 refactor: expose additional schema validation error data

--- tools/schema-option-transform.d.ts
+++ tools/schema-option-transform.d.ts
@@ -11,6 +11,7 @@ import { SchematicDescription } from '../src';
 import { FileSystemCollectionDescription, FileSystemSchematicDescription } from './description';
 export declare type SchematicDesc = SchematicDescription<FileSystemCollectionDescription, FileSystemSchematicDescription>;
 export declare class InvalidInputOptions extends BaseException {
-    constructor(options: any, errors: string[]);
+    readonly errors: schema.SchemaValidatorError[];
+    constructor(options: any, errors: schema.SchemaValidatorError[]);
 }
 export declare function validateOptionsWithSchema(registry: schema.SchemaRegistry): <T extends {}>(schematic: SchematicDescription<FileSystemCollectionDescription, FileSystemSchematicDescription>, options: T) => Observable<T>;
