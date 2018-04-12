8b3328804c00d4f2f25642c0633cbc402cf85a32
Angular Builds <circleci@angular.io>
2018-04-12 23:14:45 +0000

    799b2fa refactor: rebase schematic option transformer exception

--- tools/schema-option-transform.d.ts
+++ tools/schema-option-transform.d.ts
@@ -5,13 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { BaseException, schema } from '@angular-devkit/core';
+import { schema } from '@angular-devkit/core';
 import { Observable } from 'rxjs';
 import { SchematicDescription } from '../src';
 import { FileSystemCollectionDescription, FileSystemSchematicDescription } from './description';
 export declare type SchematicDesc = SchematicDescription<FileSystemCollectionDescription, FileSystemSchematicDescription>;
-export declare class InvalidInputOptions extends BaseException {
-    readonly errors: schema.SchemaValidatorError[];
-    constructor(options: any, errors: schema.SchemaValidatorError[]);
+export declare class InvalidInputOptions<T = {}> extends schema.SchemaValidationException {
+    constructor(options: T, errors: schema.SchemaValidatorError[]);
 }
 export declare function validateOptionsWithSchema(registry: schema.SchemaRegistry): <T extends {}>(schematic: SchematicDescription<FileSystemCollectionDescription, FileSystemSchematicDescription>, options: T) => Observable<T>;
