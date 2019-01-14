8b77c578c01b92a387d45b96a401d5914d33f572
Angular Builds <circleci@angular.io>
2019-01-14 21:10:21 +0000

    8d177e8 feat(@angular-devkit/schematics): support calling a task by file path when using a FileSystemEngineHost

--- tools/file-system-engine-host.d.ts
+++ tools/file-system-engine-host.d.ts
@@ -1,4 +1,5 @@
-import { RuleFactory } from '../src';
+import { Observable } from 'rxjs';
+import { RuleFactory, TaskExecutor } from '../src';
 import { FileSystemCollectionDesc, FileSystemSchematicDesc } from './description';
 import { FileSystemEngineHostBase } from './file-system-engine-host-base';
 /**
@@ -15,4 +16,6 @@ export declare class FileSystemEngineHost extends FileSystemEngineHostBase {
     } | null;
     protected _transformCollectionDescription(name: string, desc: Partial<FileSystemCollectionDesc>): FileSystemCollectionDesc;
     protected _transformSchematicDescription(name: string, _collection: FileSystemCollectionDesc, desc: Partial<FileSystemSchematicDesc>): FileSystemSchematicDesc;
+    hasTaskExecutor(name: string): boolean;
+    createTaskExecutor(name: string): Observable<TaskExecutor>;
 }
