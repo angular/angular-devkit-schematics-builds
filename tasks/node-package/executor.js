8cc3ab4560287036cf189a6b9250c9e9c1402be5
Angular Builds <circleci@angular.io>
2019-10-29 01:24:48 +0000

    325dcd005 fix(@angular-devkit/schematics): show spinner for package install

--- tasks/node-package/executor.js
+++ tasks/node-package/executor.js
@@ -9,8 +9,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const core_1 = require("@angular-devkit/core");
 const child_process_1 = require("child_process");
+const ora = require("ora");
 const path = require("path");
 const rxjs_1 = require("rxjs");
+const src_1 = require("../../src");
 const packageManagers = {
     'npm': {
         quietArgument: '--quiet',
@@ -62,10 +64,9 @@ function default_1(factoryOptions = {}) {
             }
             taskPackageManagerName = options.packageManager;
         }
-        const outputStream = process.stdout;
-        const errorStream = process.stderr;
+        const bufferedOutput = [];
         const spawnOptions = {
-            stdio: [process.stdin, outputStream, errorStream],
+            stdio: !!options.hideOutput ? 'pipe' : 'inherit',
             shell: true,
             cwd: path.join(rootDirectory, options.workingDirectory || ''),
         };
@@ -83,17 +84,31 @@ function default_1(factoryOptions = {}) {
             args.push(taskPackageManagerProfile.quietArgument);
         }
         return new rxjs_1.Observable(obs => {
-            child_process_1.spawn(taskPackageManagerName, args, spawnOptions)
+            const spinner = ora({
+                text: 'Installing packages...',
+                // Workaround for https://github.com/sindresorhus/ora/issues/136.
+                discardStdin: process.platform != 'win32',
+            }).start();
+            const childProcess = child_process_1.spawn(taskPackageManagerName, args, spawnOptions)
                 .on('close', (code) => {
                 if (code === 0) {
+                    spinner.succeed('Packages installed successfully.');
+                    spinner.stop();
                     obs.next();
                     obs.complete();
                 }
                 else {
-                    const message = 'Package install failed, see above.';
-                    obs.error(new Error(message));
+                    if (options.hideOutput) {
+                        bufferedOutput.forEach(({ stream, data }) => stream.write(data));
+                    }
+                    spinner.fail('Package install failed, see above.');
+                    obs.error(new src_1.UnsuccessfulWorkflowExecution());
                 }
             });
+            if (options.hideOutput) {
+                childProcess.stdout.on('data', (data) => bufferedOutput.push({ stream: process.stdout, data: data }));
+                childProcess.stderr.on('data', (data) => bufferedOutput.push({ stream: process.stderr, data: data }));
+            }
         });
     };
 }
