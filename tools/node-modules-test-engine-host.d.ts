45a1a56e5dffc0578e4ebd4f5232e6956840d50a
Angular Builds <circleci@angular.io>
2020-01-14 06:56:55 +0000

    76865f3ab build: update terser to version 4.6.3

--- tools/node-modules-test-engine-host.d.ts
+++ tools/node-modules-test-engine-host.d.ts
@@ -15,7 +15,7 @@ import { NodeModulesEngineHost } from './node-module-engine-host';
 export declare class NodeModulesTestEngineHost extends NodeModulesEngineHost {
     private _collections;
     private _tasks;
-    readonly tasks: TaskConfiguration<{}>[];
+    get tasks(): TaskConfiguration<{}>[];
     clearTasks(): void;
     registerCollection(name: string, path: string): void;
     transformContext(context: FileSystemSchematicContext): FileSystemSchematicContext;
