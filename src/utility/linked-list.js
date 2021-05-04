63423f8995ba444bbd581b9a09a131e9b238b823
Angular Builds <circleci@angular.io>
2021-05-04 14:44:26 +0000

    07a648789 build: update all non-major dependencies

--- src/utility/linked-list.js
+++ src/utility/linked-list.js
@@ -1,6 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LinkedList = void 0;
 /**
  * @license
  * Copyright Google LLC All Rights Reserved.
@@ -8,6 +6,8 @@ exports.LinkedList = void 0;
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = void 0;
 class LinkedList {
     constructor(_head) {
         this._head = _head;
