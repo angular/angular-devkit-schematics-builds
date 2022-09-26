c33f0f43b91b93758a73c3e6ec90aded6e60eeba
Angular Builds <circleci@angular.io>
2022-09-26 11:02:45 +0000

    aeb52338b test: update schematics API golden file

--- tools/fallback-engine-host.d.ts
+++ tools/fallback-engine-host.d.ts
@@ -5,7 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <reference types="node" />
 import { Observable } from 'rxjs';
 import { Url } from 'url';
 import { CollectionDescription, EngineHost, RuleFactory, SchematicDescription, Source, TaskExecutor, TypedSchematicContext } from '../src';
--- tools/file-system-engine-host-base.d.ts
+++ tools/file-system-engine-host-base.d.ts
@@ -5,7 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <reference types="node" />
 import { BaseException } from '@angular-devkit/core';
 import { Observable } from 'rxjs';
 import { Url } from 'url';
