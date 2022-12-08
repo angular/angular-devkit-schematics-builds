f3bbe1d94df8629e8d9b394ca2d5c51b538247e7
Angular Builds <circleci@angular.io>
2022-12-08 23:12:50 +0000

    27b22b02d fix(@angular-devkit/build-angular): include sources in generated Sass source maps

--- testing/schematic-test-runner.d.ts
+++ testing/schematic-test-runner.d.ts
@@ -23,7 +23,15 @@ export declare class SchematicTestRunner {
     get logger(): logging.Logger;
     get tasks(): TaskConfiguration[];
     registerCollection(collectionName: string, collectionPath: string): void;
+    runSchematic<SchematicSchemaT extends object>(schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Promise<UnitTestTree>;
+    /**
+     * @deprecated since version 15.1. Use `runSchematic` instead.
+     */
     runSchematicAsync<SchematicSchemaT extends object>(schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Observable<UnitTestTree>;
+    runExternalSchematic<SchematicSchemaT extends object>(collectionName: string, schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Promise<UnitTestTree>;
+    /**
+     * @deprecated since version 15.1. Use `runExternalSchematic` instead.
+     */
     runExternalSchematicAsync<SchematicSchemaT extends object>(collectionName: string, schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Observable<UnitTestTree>;
     callRule(rule: Rule, tree: Tree, parentContext?: Partial<SchematicContext>): Observable<Tree>;
 }
