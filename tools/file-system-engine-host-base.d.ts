c814750b5d7822a3f450f2a97fa73661c8cd1ebb
Angular Builds <circleci@angular.io>
2023-11-22 20:03:40 +0000

    3810bd4 build: update to TypeScript 5.3

--- tools/file-system-engine-host-base.d.ts
+++ tools/file-system-engine-host-base.d.ts
@@ -5,6 +5,8 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <reference types="@types/node/url" />
+/// <reference types="@types/node/ts4.8/url" />
 import { BaseException } from '@angular-devkit/core';
 import { Observable } from 'rxjs';
 import { Url } from 'url';
