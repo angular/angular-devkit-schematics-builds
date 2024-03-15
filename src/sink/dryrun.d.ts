af4009f82139b24305a0a806715a4210b5602d10
Angular Builds <circleci@angular.io>
2024-03-15 09:20:58 +0000

    c6c4364 test: update API golden files

--- src/sink/dryrun.d.ts
+++ src/sink/dryrun.d.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <reference types="node" />
 import { virtualFs } from '@angular-devkit/core';
 import { Observable, Subject } from 'rxjs';
 import { HostSink } from './host';
--- src/tree/recorder.d.ts
+++ src/tree/recorder.d.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <reference types="node" />
 import { BaseException } from '@angular-devkit/core';
 import MagicString from 'magic-string';
 import { FileEntry, UpdateRecorder } from './interface';
